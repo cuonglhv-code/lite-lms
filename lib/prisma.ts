a491ab8946894d131f021c2312e527b42f3b490e
cuonglhv-code <cuonglhv@gmail.com>
2026-03-22 19:28:19 +0000

    up

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient | undefined
-
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient()
-  }
-  prisma = globalForPrisma.prisma
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.POSTGRES_PRISMA_URL,
+    },
+  },
+})
 
-export default prisma as PrismaClient
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
 
